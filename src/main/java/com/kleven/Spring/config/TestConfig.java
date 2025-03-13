2f34b9352e9268282ccd4348d52f60fe130e5954
Daniele Kleven <daniilopes.91@gmail.com>
2025-03-13 15:58:01 +0100

    fixing

--- src/main/java/com/kleven/Spring/config/TestConfig.java
+++ src/main/java/com/kleven/Spring/config/TestConfig.java
@@ -7,14 +7,13 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
-import java.lang.reflect.Array;
 import java.util.Arrays;
 
 @Configuration
 @Profile("test")
-public class testconfig implements CommandLineRunner {
+public class TestConfig implements CommandLineRunner {
     @Autowired
-    UserRepository repository;
+    private UserRepository repository;
 
     @Override
     public void run(String... args) throws Exception {
