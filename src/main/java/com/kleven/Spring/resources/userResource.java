ff8a75d5b653b55f5d6afafffd9e995faf05c784
Daniele Kleven <daniilopes.91@gmail.com>
2025-03-13 14:48:53 +0100

    user entities and resources

---
+++ src/main/java/com/kleven/Spring/resources/userResource.java
@@ -0,0 +1,18 @@
+package com.kleven.Spring.resources;
+
+import com.kleven.Spring.entities.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/users")
+public class userResource {
+
+    @GetMapping
+    public ResponseEntity<User> findAll() {
+        User u = new User(1, "Maria","maria@dores.com","9999999","pass");
+        return ResponseEntity.ok().body(u);
+    }
+}
