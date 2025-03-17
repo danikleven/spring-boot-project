da6084d89f58098996caf4d9b2dd97d5edd2438f
Daniele Kleven <daniilopes.91@gmail.com>
2025-03-17 10:50:27 +0100

    Service layer, component registration

---
+++ src/main/java/com/kleven/Spring/service/UserService.java
@@ -0,0 +1,26 @@
+package com.kleven.Spring.service;
+
+import com.kleven.Spring.entities.User;
+import com.kleven.Spring.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository repository;
+
+    public List<User> findAll(){
+        return repository.findAll();
+    }
+
+    public User findById(Long id){
+        Optional<User> obj = repository.findById(id);
+        return obj.get();
+    }
+}
